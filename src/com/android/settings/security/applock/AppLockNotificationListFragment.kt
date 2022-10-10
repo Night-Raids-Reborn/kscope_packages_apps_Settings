AOSP-Krypton/packages_apps_Settings

Settings: introduce app lock [3/4]
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

src/com/android/settings/security/applock/AppLockNotificationListFragment.kt
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.os.Bundle
+import android.view.View
+
+import com.android.settings.R
+import com.krypton.settings.fragment.AppListFragment
+
+class AppLockNotificationListFragment : AppListFragment() {
+
+    private lateinit var appLockManager: AppLockManager
+    private val lockedPackages = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appLockManager = requireContext().getSystemService(AppLockManager::class.java)
+        lockedPackages.addAll(appLockManager.getPackages())
+    }
+
+    override protected fun getTitle(): Int = R.string.app_lock_notifications_title
+
+    override protected fun getInitialCheckedList(): List<String> =
+        appLockManager.getPackagesWithSecureNotifications()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setDisplayCategory(CATEGORY_BOTH)
+        setCustomFilter {
+            lockedPackages.contains(it.packageName)
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override protected fun onAppSelected(packageName: String) {
+        appLockManager.setSecureNotification(packageName, true)
+    }
+
+    override protected fun onAppDeselected(packageName: String) {
+        appLockManager.setSecureNotification(packageName, false)
+    }
+}

src/com/android/settings/security/applock/AppLockNotificationPreferenceController.kt
@@ -1,0 +1,77 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.os.UserHandle
+
+import androidx.lifecycle.Lifecycle.Event
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.internal.widget.LockPatternUtils
+import com.android.settings.R
+import com.android.settingslib.core.lifecycle.Lifecycle
+import com.krypton.settings.KryptonBasePreferenceController
+
+class AppLockNotificationPreferenceController(
+    private val context: Context,
+    lifecycle: Lifecycle?,
+) : KryptonBasePreferenceController(context, KEY),
+    LifecycleEventObserver {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+
+    private var preference: Preference? = null
+
+    init {
+        lifecycle?.addObserver(this)
+    }
+
+    override fun getAvailabilityStatus() =
+        if (appLockManager.getPackages().isNotEmpty()) AVAILABLE else DISABLED_DEPENDENT_SETTING
+
+    override fun onStateChanged(owner: LifecycleOwner, event: Event) {
+        if (event == Event.ON_START) {
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+
+    override fun updateState(preference: Preference) {
+        if (getAvailabilityStatus() == AVAILABLE) {
+            preference.setEnabled(true)
+            preference.summary = context.getString(R.string.app_lock_notifications_summary)
+        } else {
+            preference.setEnabled(false)
+            preference.summary = context.getString(R.string.app_lock_notifications_disabled_summary)
+        }
+    }
+
+    companion object {
+        private const val KEY = "app_lock_notifications"
+    }
+}

src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.os.Bundle
+import android.view.View
+
+import com.android.settings.R
+import com.krypton.settings.fragment.AppListFragment
+
+class AppLockPackageListFragment : AppListFragment() {
+
+    private lateinit var appLockManager: AppLockManager
+    private val whiteListedPackages = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appLockManager = requireContext().getSystemService(AppLockManager::class.java)
+        whiteListedPackages.addAll(requireContext().resources.getStringArray(
+            com.android.internal.R.array.config_appLockAllowedSystemApps).toList())
+    }
+
+    override protected fun getTitle(): Int = R.string.app_lock_packages_title
+
+    override protected fun getInitialCheckedList(): List<String> = appLockManager.getPackages()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setDisplayCategory(CATEGORY_BOTH)
+        setCustomFilter {
+            !it.applicationInfo.isSystemApp() || whiteListedPackages.contains(it.packageName)
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override protected fun onAppSelected(packageName: String) {
+        appLockManager.addPackage(packageName)
+    }
+
+    override protected fun onAppDeselected(packageName: String) {
+        appLockManager.removePackage(packageName)
+    }
+}

src/com/android/settings/security/applock/AppLockSettingsFragment.kt
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.core.lifecycle.Lifecycle
+import com.android.settingslib.search.SearchIndexable
+import com.krypton.settings.fragment.KryptonDashboardFragment
+
+@SearchIndexable
+class AppLockSettingsFragment : KryptonDashboardFragment() {
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
+
+    override protected fun getLogTag() = TAG
+
+    override protected fun createPreferenceControllers(context: Context) =
+        buildPreferenceControllers(context, settingsLifecycle)
+
+    companion object {
+        private const val TAG = "AppLockSettingsFragment"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = object : BaseSearchIndexProvider(
+            R.xml.app_lock_settings
+        ) {
+            override fun createPreferenceControllers(context: Context) =
+                buildPreferenceControllers(context)
+        }
+
+        fun buildPreferenceControllers(
+            context: Context,
+            lifecycle: Lifecycle? = null
+        ): List<AbstractPreferenceController> =
+            listOf(
+                AppLockNotificationPreferenceController(
+                    context,
+                    lifecycle,
+                )
+            )
+    }
+}

src/com/android/settings/security/applock/AppLockTimeoutPreferenceController.kt
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+
+import com.krypton.settings.KryptonBasePreferenceController
+
+class AppLockTimeoutPreferenceController(
+    context: Context,
+    key: String,
+) : KryptonBasePreferenceController(context, key),
+        Preference.OnPreferenceChangeListener {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun updateState(preference: Preference) {
+        val timeout = appLockManager.getTimeout()
+        (preference as ListPreference).value = if (timeout == -1L) null else timeout.toString()
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        appLockManager.setTimeout((newValue as String).toLong())
+        return true
+    }
+}

Night-Raids-Reborn/kscope_packages_apps_Settings

Change-Id: I9945fee2e29024abc77e692c917ae0c22a354003

src/com/android/settings/security/applock/AppLockNotificationListFragment.kt
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.os.Bundle
+import android.view.View
+
+import com.android.settings.R
+import com.android.settings.custom.AppListFragment
+
+class AppLockNotificationListFragment : AppListFragment() {
+
+    private lateinit var appLockManager: AppLockManager
+    private val lockedPackages = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appLockManager = requireContext().getSystemService(AppLockManager::class.java)
+        lockedPackages.addAll(appLockManager.getPackages())
+    }
+
+    override protected fun getTitle(): Int = R.string.app_lock_notifications_title
+
+    override protected fun getInitialCheckedList(): List<String> =
+        appLockManager.getPackagesWithSecureNotifications()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setDisplayCategory(CATEGORY_BOTH)
+        setCustomFilter {
+            lockedPackages.contains(it.packageName)
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override protected fun onAppSelected(packageName: String) {
+        appLockManager.setSecureNotification(packageName, true)
+    }
+
+    override protected fun onAppDeselected(packageName: String) {
+        appLockManager.setSecureNotification(packageName, false)
+    }
+}

src/com/android/settings/security/applock/AppLockNotificationPreferenceController.kt
@@ -1,0 +1,77 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.os.UserHandle
+
+import androidx.lifecycle.Lifecycle.Event
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.preference.Preference
+import androidx.preference.PreferenceScreen
+
+import com.android.internal.widget.LockPatternUtils
+import com.android.settings.R
+import com.android.settingslib.core.lifecycle.Lifecycle
+import com.android.settings.core.BasePreferenceController
+
+class AppLockNotificationPreferenceController(
+    private val context: Context,
+    lifecycle: Lifecycle?,
+) : BasePreferenceController(context, KEY),
+    LifecycleEventObserver {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+
+    private var preference: Preference? = null
+
+    init {
+        lifecycle?.addObserver(this)
+    }
+
+    override fun getAvailabilityStatus() =
+        if (appLockManager.getPackages().isNotEmpty()) AVAILABLE else DISABLED_DEPENDENT_SETTING
+
+    override fun onStateChanged(owner: LifecycleOwner, event: Event) {
+        if (event == Event.ON_START) {
+            preference?.let {
+                updateState(it)
+            }
+        }
+    }
+
+    override fun displayPreference(screen: PreferenceScreen) {
+        super.displayPreference(screen)
+        preference = screen.findPreference(preferenceKey)
+    }
+
+    override fun updateState(preference: Preference) {
+        if (getAvailabilityStatus() == AVAILABLE) {
+            preference.setEnabled(true)
+            preference.summary = context.getString(R.string.app_lock_notifications_summary)
+        } else {
+            preference.setEnabled(false)
+            preference.summary = context.getString(R.string.app_lock_notifications_disabled_summary)
+        }
+    }
+
+    companion object {
+        private const val KEY = "app_lock_notifications"
+    }
+}

src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.os.Bundle
+import android.view.View
+
+import com.android.settings.R
+import com.android.settings.custom.AppListFragment
+
+class AppLockPackageListFragment : AppListFragment() {
+
+    private lateinit var appLockManager: AppLockManager
+    private val whiteListedPackages = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appLockManager = requireContext().getSystemService(AppLockManager::class.java)
+        whiteListedPackages.addAll(requireContext().resources.getStringArray(
+            com.android.internal.R.array.config_appLockAllowedSystemApps).toList())
+    }
+
+    override protected fun getTitle(): Int = R.string.app_lock_packages_title
+
+    override protected fun getInitialCheckedList(): List<String> = appLockManager.getPackages()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setDisplayCategory(CATEGORY_BOTH)
+        setCustomFilter {
+            !it.applicationInfo.isSystemApp() || whiteListedPackages.contains(it.packageName)
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override protected fun onAppSelected(packageName: String) {
+        appLockManager.addPackage(packageName)
+    }
+
+    override protected fun onAppDeselected(packageName: String) {
+        appLockManager.removePackage(packageName)
+    }
+}

src/com/android/settings/security/applock/AppLockSettingsFragment.kt
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import com.android.internal.logging.nano.MetricsProto
+
+import android.content.Context
+
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.core.lifecycle.Lifecycle
+import com.android.settingslib.search.SearchIndexable
+import com.android.settings.dashboard.DashboardFragment
+
+@SearchIndexable
+class AppLockSettingsFragment : DashboardFragment() {
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
+
+    override protected fun getLogTag() = TAG
+
+    override fun getMetricsCategory(): Int = 1800
+
+    override protected fun createPreferenceControllers(context: Context) =
+        buildPreferenceControllers(context, settingsLifecycle)
+
+    companion object {
+        private const val TAG = "AppLockSettingsFragment"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = object : BaseSearchIndexProvider(
+            R.xml.app_lock_settings
+        ) {
+            override fun createPreferenceControllers(context: Context) =
+                buildPreferenceControllers(context)
+        }
+
+        fun buildPreferenceControllers(
+            context: Context,
+            lifecycle: Lifecycle? = null
+        ): List<AbstractPreferenceController> =
+            listOf(
+                AppLockNotificationPreferenceController(
+                    context,
+                    lifecycle,
+                )
+            )
+    }
+}

src/com/android/settings/security/applock/AppLockTimeoutPreferenceController.kt
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+
+import com.android.settings.core.BasePreferenceController
+
+class AppLockTimeoutPreferenceController(
+    context: Context,
+    key: String,
+) : BasePreferenceController(context, key),
+        Preference.OnPreferenceChangeListener {
+
+    private val appLockManager = context.getSystemService(AppLockManager::class.java)
+
+    override fun getAvailabilityStatus() = AVAILABLE
+
+    override fun updateState(preference: Preference) {
+        val timeout = appLockManager.getTimeout()
+        (preference as ListPreference).value = if (timeout == -1L) null else timeout.toString()
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        appLockManager.setTimeout((newValue as String).toLong())
+        return true
+    }
+}